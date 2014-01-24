f8e940297b3a7125d7bfb6e0df9d18a109f6ba73
ljleppan <leo.leppanen@helsinki.fi>
2014-01-24 14:13:07 +0200

    Initial tests for Document

---
+++ nllr/src/test/java/loez/nllr/domain/DocumentTest.java
@@ -0,0 +1,58 @@
+package loez.nllr.domain;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import junit.framework.TestCase;
+
+/**
+ * Tests for Document
+ * @author ljleppan@cs
+ */
+public class DocumentTest extends TestCase {
+    private Document doc;
+    
+    
+    @Override
+    protected void setUp() throws Exception {
+        String docBody = "auto asia auto auto auto";
+        Calendar docDate = new GregorianCalendar();
+        doc = new Document(docDate, docBody);
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void testDatelessConstructorDoesNotGenerateDates(){
+        Document d = new Document("asd");
+        assertTrue("Creating a new Document without a date resulted in a non-null date",
+                d.getDate() == null);
+
+    }
+    
+    public void testGetUniqueTokens(){
+        String[] uniqueTokens = doc.getUniqueTokens();
+        assertTrue("getUniqueTokens() returned an incorrect amount of unique tokens.",
+                uniqueTokens.length == 2);
+        assertTrue("getUniqueTokens() should return an array that includes the String \"asia\"",
+                (uniqueTokens[0].equals("asia") || uniqueTokens[1].equals("asia")));
+        assertTrue("getUniqueTokens() should return an array that includes the String \"auto\"",
+                (uniqueTokens[0].equals("auto") || uniqueTokens[1].equals("auto")));
+    }
+    
+    public void testGetTotalTokens(){
+        assertTrue("getTotalTokens should return 5 when the document body has 5 tokens", 
+               doc.getTotalTokens() == 5);
+    }
+    
+    public void testGetFrequency(){
+        assertTrue("getFrequency should return 4 when the document has 4 of the queried token",
+                doc.getFrequency("auto") == 4);
+        assertTrue("getFrequency should return 1 when the document has 1 of the queried token",
+                doc.getFrequency("asia") == 1);
+        assertTrue("getFrequency should return 0 when the document has 0 of the queried token",
+                doc.getFrequency("tokenThatDoesNotExist") == 0);
+    }
+}
