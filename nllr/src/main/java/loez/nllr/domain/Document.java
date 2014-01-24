c0b812f5c0f46ce364f20ab7b8e4d54300078f84
ljleppan <leo.leppanen@helsinki.fi>
2014-01-24 13:18:12 +0200

    Initial version of Document class

---
+++ nllr/src/main/java/loez/nllr/domain/Document.java
@@ -0,0 +1,83 @@
+package loez.nllr.domain;
+
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * Document is a single text.
+ * @author ljleppan@cs
+ */
+public class Document {
+    private String body;
+    private Date date;
+    private HashMap<String, Integer> tokenAmounts;
+    private int numTokens;
+    
+    /**
+     * Creates a new document with known date.
+     * @param date  Date of document's creation.
+     * @param body  The text of the document.
+     */
+    public Document(Date date, String body){
+        this.body = body;
+        this.date = date;
+        countTokenFrequencies();
+    }
+    
+    /**
+     * Creates a new document without a known date.
+     * @param body  The text of the document.
+     */
+    public Document(String body){
+        this(null, body);
+    }
+
+    private void countTokenFrequencies() {
+        numTokens = 0;
+        
+        String[] tokens = body.split(" ");
+        for (String t : tokens){
+            numTokens++;
+            
+            if (tokenAmounts.containsKey(t)){
+                int amountNow = tokenAmounts.get(t);
+                tokenAmounts.put(t, amountNow + 1);
+            } else {
+                tokenAmounts.put(t, 1);
+            }
+        }
+    }
+    
+    /**
+     * Get the frequency of a given token in the document.
+     * @param token The query token.
+     * @return      The frequency of the token.
+     */
+    public int getFrequency(String token){
+        return tokenAmounts.get(token);
+    }
+    
+    /**
+     * Get the total amount of tokens (non-unique) in the document.
+     * @return  The total amount of token in the document body.
+     */
+    public int getTotalTokens(){
+        return numTokens;
+    }
+    
+    /**
+     * Get the document's unique tokens.
+     * @return  An array of unique tokens in the documents. Order is not specified.
+     */
+    public String[] getUniqueTokens(){
+        return tokenAmounts.keySet().toArray(new String[tokenAmounts.keySet().size()]);
+    }
+    
+    /**
+     * Get the date of creation of the document.
+     * @return The date of creation.
+     */
+    public Date getDate(){
+        return date;
+    }
+}
