0a1165552f2b2fa3fc70eff5c9c61f7e308d5d29
ljleppan <leo.leppanen@helsinki.fi>
2014-01-24 13:19:04 +0200

    Empty shell for the Tf-idf

---
+++ nllr/src/main/java/loez/nllr/algorithm/Tfidf.java
@@ -0,0 +1,39 @@
+package loez.nllr.algorithm;
+
+/**
+ *
+ * @author ljleppan@cs
+ */
+public class Tfidf {
+
+    /**
+     *
+     * @param numTokens
+     * @param numDocumentTotalTokens
+     * @return
+     */
+    public static int tf(int numTokens, int numDocumentTotalTokens){
+        return -1;
+    }
+    
+    /**
+     *
+     * @param numTokens
+     * @param numCorpusTotalTokens
+     * @return
+     */
+    public static int idf(int numTokens, int numCorpusTotalTokens){
+        return -1;
+    }
+    
+    /**
+     *
+     * @param numTokens
+     * @param numDocumentTotalTokens
+     * @param numCorpusTotalTokens
+     * @return
+     */
+    public static int tfidf(int numTokens, int numDocumentTotalTokens, int numCorpusTotalTokens){
+        return -1;
+    }
+}
