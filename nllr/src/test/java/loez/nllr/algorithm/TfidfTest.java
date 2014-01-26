452f0439a78e59756383249f3e6baa58dcb4dbfb
ljleppan <leo.leppanen@helsinki.fi>
2014-01-26 14:59:07 +0200

    Empty unit test file for tf-idf

---
+++ nllr/src/test/java/loez/nllr/algorithm/TfidfTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package loez.nllr.algorithm;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author loezi
+ */
+public class TfidfTest extends TestCase{
+    
+    @Override
+    public void setUp() {
+        
+    }
+    
+    
+}
