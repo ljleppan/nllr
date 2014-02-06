9da87f99f2c991de4652b08b69741f0937c133b8
ljleppan <leo.leppanen@helsinki.fi>
2014-02-06 19:35:55 +0200

    Added Javadoc for BagOfWords

--- nllr/src/main/java/loez/nllr/domain/BagOfWords.java
+++ nllr/src/main/java/loez/nllr/domain/BagOfWords.java
@@ -3,11 +3,25 @@ package loez.nllr.domain;
 import loez.nllr.datastructure.HashSet;
 
 /**
- *
+ * A bag of words.
+ * Represents a non-ordered group of tokens.
  * @author loezi
  */
 public interface BagOfWords {
+
+    /**
+     * @return Total amount of non-unique token in the bag.
+     */
     public int getTotalTokens();
+
+    /**
+     * @param token A token in the bag.
+     * @return      The frequency of queried token in bag.
+     */
     public int getFrequency(String token);
+
+    /**
+     * @return  A HashSet containing all unique tokens found in the bag.
+     */
     public HashSet<String> getUniqueTokens();
 }
