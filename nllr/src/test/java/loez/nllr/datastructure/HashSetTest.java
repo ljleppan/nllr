b8e53141871e5b3fadb7b223ec3241e25e65c821
ljleppan <leo.leppanen@helsinki.fi>
2014-02-06 19:29:08 +0200

    Removed unused imports from HashSetTest

--- nllr/src/test/java/loez/nllr/datastructure/HashSetTest.java
+++ nllr/src/test/java/loez/nllr/datastructure/HashSetTest.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package loez.nllr.datastructure;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
