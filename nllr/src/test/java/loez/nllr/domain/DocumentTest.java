8aacf4a115ad86c7c6ffc2a3cf447061d0fd8fd0
ljleppan <leo.leppanen@helsinki.fi>
2014-02-06 17:48:54 +0200

    Updated some Document test failure messages to reflect the change from arraylist to hashmap in Document.getUniqueTokens

--- nllr/src/test/java/loez/nllr/domain/DocumentTest.java
+++ nllr/src/test/java/loez/nllr/domain/DocumentTest.java
@@ -2,57 +2,58 @@ package loez.nllr.domain;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import junit.framework.TestCase;
 import loez.nllr.datastructure.HashSet;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Tests for Document
  * @author ljleppan@cs
  */
-public class DocumentTest extends TestCase {
+public class DocumentTest {
     private Document doc;
     
     
-    @Override
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         String docBody = "auto asia auto auto auto";
         Calendar docDate = new GregorianCalendar();
         doc = new Document(docDate, docBody);
     }
     
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-    
+    @Test
     public void testDatelessConstructorDoesNotGenerateDates(){
         Document d = new Document("asd");
-        assertTrue("Creating a new Document without a date resulted in a non-null date",
-                d.getDate() == null);
+        assertNull("Creating a new Document without a date resulted in a non-null date",
+                d.getDate());
 
     }
     
+    @Test
     public void testGetUniqueTokens(){
         HashSet<String> uniqueTokens = doc.getUniqueTokens();
-        assertTrue("getUniqueTokens() returned an incorrect amount of unique tokens.",
-                uniqueTokens.size() == 2);
-        assertTrue("getUniqueTokens() should return an array that includes the String \"asia\"",
+        assertEquals("getUniqueTokens() returned an incorrect amount of unique tokens.",
+                uniqueTokens.size(), 2);
+        assertTrue("getUniqueTokens() should return a hashset that includes the String \"asia\"",
                 uniqueTokens.contains("asia"));
-        assertTrue("getUniqueTokens() should return an array that includes the String \"auto\"",
+        assertTrue("getUniqueTokens() should return a hashset that includes the String \"auto\"",
                 uniqueTokens.contains("auto"));
     }
     
+    @Test
     public void testGetTotalTokens(){
-        assertTrue("getTotalTokens should return 5 when the document body has 5 tokens", 
-               doc.getTotalTokens() == 5);
+        assertEquals("getTotalTokens should return 5 when the document body has 5 tokens", 
+               doc.getTotalTokens(), 5);
     }
     
+    @Test
     public void testGetFrequency(){
-        assertTrue("getFrequency should return 4 when the document has 4 of the queried token",
-                doc.getFrequency("auto") == 4);
-        assertTrue("getFrequency should return 1 when the document has 1 of the queried token",
-                doc.getFrequency("asia") == 1);
-        assertTrue("getFrequency should return 0 when the document has 0 of the queried token",
-                doc.getFrequency("tokenThatDoesNotExist") == 0);
+        assertEquals("getFrequency should return 4 when the document has 4 of the queried token",
+                doc.getFrequency("auto"), 4);
+        assertEquals("getFrequency should return 1 when the document has 1 of the queried token",
+                doc.getFrequency("asia"), 1);
+        assertEquals("getFrequency should return 0 when the document has 0 of the queried token",
+                doc.getFrequency("tokenThatDoesNotExist"), 0);
     }
 }
