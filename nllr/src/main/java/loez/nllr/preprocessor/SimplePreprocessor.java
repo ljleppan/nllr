95937fa22dcadebc6dc64798b7f5865bd084ff16
ljleppan <leo.leppanen@helsinki.fi>
2014-02-10 21:15:00 +0200

    Added the SimplePreprocessor class

---
+++ nllr/src/main/java/loez/nllr/preprocessor/SimplePreprocessor.java
@@ -0,0 +1,21 @@
+package loez.nllr.preprocessor;
+
+/**
+ * An extremely simple preprocessor, for use as a fallback.
+ * @author loezi
+ */
+public class SimplePreprocessor implements PreProcessor{
+    
+    /**
+     * Processes the input string into tokens.
+     * @param input String of words
+     * @return      String of tokens
+     */
+    @Override
+    public String process(String input){
+        String output = input.toUpperCase();
+        output = output.replaceAll("[^A-Z ]", "");
+        output = output.trim();
+        return output;
+    }
+}
