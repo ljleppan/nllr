baa6126a9ee534b39d100fe165703270684f441d
ljleppan <leo.leppanen@helsinki.fi>
2014-02-12 15:08:32 +0200

    Removed prints from CorpusReader

--- nllr/src/main/java/loez/nllr/reader/CorpusReader.java
+++ nllr/src/main/java/loez/nllr/reader/CorpusReader.java
@@ -25,23 +25,15 @@ public class CorpusReader {
         try(BufferedReader in = new BufferedReader(new FileReader(path))) {
             String rawDocumentString;
             Corpus corpus = new Corpus();
-            System.out.println("Parsing corpus");
-            int i = 0;
             while ((rawDocumentString = in.readLine()) != null){
-                System.out.print("Document "+ (++i) +" ... ");
                 Document document = DocumentConverter.rawStringToDocument(rawDocumentString, dateFormat, preprocessor);
                 if (document != null){
                     corpus.add(document);
-                    System.out.print("succesfull.");
                 }
-                System.out.println("");
             }
-
             return corpus;
         } catch (IOException e) {
             return null;
         }
     }
-    
-    
 }
