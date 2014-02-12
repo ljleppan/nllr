667737d828f1dce7ba60b673f49469fb0943ae2c
ljleppan <leo.leppanen@helsinki.fi>
2014-02-12 14:38:28 +0200

    Added PreProcessorUtil interface to tie together Preprocessor utils

---
+++ nllr/src/main/java/loez/nllr/preprocessor/util/PreprocessorUtil.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package loez.nllr.preprocessor.util;
+
+/**
+ *
+ * @author ljleppan
+ */
+public interface PreprocessorUtil {
+    public String replace(String input, String replacement);
+    public String remove(String input);
+}
