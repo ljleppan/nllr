3152f29185cc88eb9f91da381586e3840aed4aba
ljleppan <leo.leppanen@helsinki.fi>
2014-02-12 14:40:25 +0200

    Added tests for SimplePreprocessor setLanguage() and getLanguage()

--- nllr/src/test/java/loez/nllr/preprocessor/SimplePreprocessorTest.java
+++ nllr/src/test/java/loez/nllr/preprocessor/SimplePreprocessorTest.java
@@ -10,14 +10,25 @@ import org.junit.Test;
 public class SimplePreprocessorTest {
     private SimplePreprocessor pp = new SimplePreprocessor();
 
-
+    @Test
+    public void canCallSetLanguageWithoutException(){
+        pp.setLanguage("");
+    }
+    
+    @Test
+    public void getLanguage(){
+        assertEquals("Simple preprocessor should return correct string when getLanguage is called",
+                "Simple preprocessor is language independent",
+                pp.getLanguage());
+    }
+    
     @Test
     public void correctlyProcesses(){
         assertEquals("a string should be correctly processed",
                 "THIS IS A STRING",
                 pp.process("tHiS, is; a string...."));
         assertEquals("a string should be correctly processed",
-                "ALSO A STRING",
-                pp.process("a.l.so A- string"));
+                "ALSO A STRING THIS TIME WITH NUMERAL",
+                pp.process("a.l.so A- string this time with 1233321312"));
     }
 }
