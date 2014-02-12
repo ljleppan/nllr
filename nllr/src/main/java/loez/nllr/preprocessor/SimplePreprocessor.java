6ccaa3d8b9969a71d6ea04546ce71da8d07d901a
ljleppan <leo.leppanen@helsinki.fi>
2014-02-12 14:36:44 +0200

    Refactored SimplePreprocessor to use new utils

--- nllr/src/main/java/loez/nllr/preprocessor/SimplePreprocessor.java
+++ nllr/src/main/java/loez/nllr/preprocessor/SimplePreprocessor.java
@@ -1,10 +1,15 @@
 package loez.nllr.preprocessor;
 
+import loez.nllr.preprocessor.util.Numeral;
+import loez.nllr.preprocessor.util.Punctuation;
+
 /**
  * An extremely simple preprocessor, for use as a fallback.
  * @author loezi
  */
 public class SimplePreprocessor implements PreProcessor{
+    private final Punctuation punctuation = new Punctuation();
+    private final Numeral numeral = new Numeral();
     
     /**
      * Processes the input string into tokens.
@@ -13,9 +18,10 @@ public class SimplePreprocessor implements PreProcessor{
      */
     @Override
     public String process(String input){
-        String output = input.toUpperCase();
-        output = output.replaceAll("[^A-Z ]", "");
-        output = output.trim();
+        String output = input;
+        output = punctuation.remove(output);
+        output = numeral.replace(output, "NUMERAL");
+        output = output.trim().toUpperCase();
         return output;
     }
     
