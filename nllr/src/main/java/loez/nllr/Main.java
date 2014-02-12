944f97f04cc2867d7dc0edbc538dc663c979475d
ljleppan <leo.leppanen@helsinki.fi>
2014-02-12 14:44:01 +0200

    Added Snowball to CLI initialization

--- nllr/src/main/java/loez/nllr/Main.java
+++ nllr/src/main/java/loez/nllr/Main.java
@@ -3,26 +3,35 @@ package loez.nllr;
 import loez.nllr.datastructure.ArrayList;
 import loez.nllr.preprocessor.PreProcessor;
 import loez.nllr.preprocessor.SimplePreprocessor;
+import loez.nllr.preprocessor.SnowballPreprocessor;
+import loez.nllr.preprocessor.exception.StemmerCreationException;
+import loez.nllr.preprocessor.util.Numeral;
+import loez.nllr.preprocessor.util.Punctuation;
 import loez.nllr.userinterface.CommandLineInterface;
 import loez.nllr.userinterface.UserInterface;
 
 public class Main 
 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws StemmerCreationException
+    {       
         UserInterface ui = new CommandLineInterface();
         
         ArrayList<String> ppNames = new ArrayList<>();
-        ppNames.add("");
-        ppNames.add("simple");
-        
         ArrayList<PreProcessor> pps = new ArrayList<>();
+        
+        ppNames.add("");
         pps.add(new SimplePreprocessor());
+        
+        ppNames.add("simple");
         pps.add(new SimplePreprocessor());
+        
+        try {
+            pps.add(new SnowballPreprocessor());
+            ppNames.add("snowball");
+        } catch (StemmerCreationException e) { }
         
         ui.setupPreprocessors(pps, ppNames);
-        
         ui.run();
     }
 }
