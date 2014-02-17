84d4b183fba0426e6c95138f39f1ca3a9f7bfb15
ljleppan <leo.leppanen@helsinki.fi>
2014-02-17 09:20:03 +0200

    Converter now outputs to a single file, since LewisSplit was not random enough and we need a custom split.

--- convert.py
+++ convert.py
@@ -10,59 +10,54 @@ file_nums = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
 
 types = ['TRAIN', 'TEST']
 
-for type in types:
-  print type
-  if not os.path.exists(os.path.dirname("reuters-fixed/")):
-    os.makedirs(os.path.dirname("reuters-fixed/"))
-  file_out = file_out_path = "reuters-fixed/" + type + ".csv"
-  file_out = open(file_out_path, "w")
+if not os.path.exists(os.path.dirname("reuters-fixed/")):
+  os.makedirs(os.path.dirname("reuters-fixed/"))
 
-  for file_num in file_nums:
-    print file_num+" / 21"
+file_out = open("reuters-fixed/reuters.csv", "w")
 
-    file_in_path = "reuters/reut2-0" + file_num + ".sgm"
-    file_in = open(file_in_path)
-    data = file_in.read().decode('utf8', errors='ignore')
-    file_in.close()
+for file_num in file_nums:
+  print file_num+" / 21"
+  file_in_path = "reuters/reut2-0" + file_num + ".sgm"
+  file_in = open(file_in_path)
+  data = file_in.read().decode('utf8', errors='ignore')
+  file_in.close()
 
-    html_data = etree.HTML(data)
-    result = etree.tostring(html_data, pretty_print=True, method="html")
+  html_data = etree.HTML(data)
+  result = etree.tostring(html_data, pretty_print=True, method="html")
 
-    parsed = html.fromstring(result)
-    for body in parsed:
-      for reuters in body:
-        split = reuters.attrib['lewissplit'].strip()
+  parsed = html.fromstring(result)
+  for body in parsed:
+    for reuters in body:
+      split = reuters.attrib['lewissplit'].strip()
+      date = reuters.find('date').text_content()
 
-        date = reuters.find('date').text_content()
+      text = reuters.find('text')
 
-        text = reuters.find('text')
+      title = text.find('title')
+      if title is None:
+        title = ""
+      else:
+        title = title.text_content().strip('\r\n')
 
-        title = text.find('title')
-        if title is None:
-          title = ""
-        else:
-          title = title.text_content().strip('\r\n')
+      dateline = text.find('dateline')
+      if dateline is None:
+        dateline = ""
+      else:
+        dateline = dateline.text_content().replace('\n', ' ').replace('\r', ' ')
 
-        dateline = text.find('dateline')
-        if dateline is None:
-          dateline = ""
-        else:
-          dateline = dateline.text_content().replace('\n', ' ').replace('\r', ' ')
+      text_body = text.text_content()
+      text_body = text_body.replace('\n', ' ').replace('\r', ' ')
+      text_body = text_body.replace(';', ' ')
+      text_body = text_body.replace(title, ' ');
+      text_body = text_body.replace(dateline, ' ');
 
-        text_body = text.text_content()
-        text_body = text_body.replace('\n', ' ').replace('\r', ' ')
-        text_body = text_body.replace(';', ' ')
-	text_body = text_body.replace(title, ' ');
-        text_body = text_body.replace(dateline, ' ');
+      line = "" + split + ";" + date + ";" + title + ";"  + text_body + "\n"
 
-        line = "" + split + ";" + date + ";" + dateline + ";" + title + ";"  + text_body + "\n"
-
-        if type == split:
-          if not "B l a h" in text_body:
-            file_out.write(line)
+      if split in types:
+        if not "B l a h" in text_body:
+          file_out.write(line)
   
-  file_out.close()
-  print "--"
+file_out.close()
 print "done.\n"
 
 
