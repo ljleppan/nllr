380e01b6916d5a1d7b47e8563c62ab0e2e1af520
ljleppan <leo.leppanen@helsinki.fi>
2014-02-17 21:36:33 +0200

    Added whitespace to TimeSpan for readability

--- nllr/src/main/java/loez/nllr/domain/TimeSpan.java
+++ nllr/src/main/java/loez/nllr/domain/TimeSpan.java
@@ -75,13 +75,17 @@ public class TimeSpan {
     public void advance(){
         if (length == Length.DAILY){
             startDate.add(Calendar.DATE, 1);
-        } else if (length == Length.WEEKLY){ 
+        } 
+        else if (length == Length.WEEKLY){ 
             startDate.add(Calendar.WEEK_OF_YEAR, 1);
-        } else if (length == Length.BIWEEKLY){ 
+        } 
+        else if (length == Length.BIWEEKLY){ 
             startDate.add(Calendar.DATE, 14);
-        } else if (length == Length.MONTHLY){
+        } 
+        else if (length == Length.MONTHLY){
             startDate.add(Calendar.MONTH, 1);
-        } else if (length == Length.YEARLY){
+        } 
+        else if (length == Length.YEARLY){
             startDate.add(Calendar.YEAR, 1);
         }
         
@@ -91,18 +95,22 @@ public class TimeSpan {
     
     private void calculateRealStartDate(Calendar rawStartDate){
         clearDate(rawStartDate);
+        rawStartDate.setFirstDayOfWeek(Calendar.MONDAY);
         
         startDate = rawStartDate;
         //Default is Length.DAILY
         
         if (length == Length.WEEKLY){
             startDate.add(Calendar.DAY_OF_WEEK, startDate.getFirstDayOfWeek() - startDate.get(Calendar.DAY_OF_WEEK));
-        } else if (length == Length.BIWEEKLY){
+        } 
+        else if (length == Length.BIWEEKLY){
             startDate.add(Calendar.DAY_OF_WEEK, startDate.getFirstDayOfWeek() - startDate.get(Calendar.DAY_OF_WEEK));
             startDate.add(Calendar.WEEK_OF_YEAR, Calendar.WEEK_OF_YEAR % 2 - 1);
-        } else if (length == Length.MONTHLY){
+        } 
+        else if (length == Length.MONTHLY){
             startDate.set(Calendar.DAY_OF_MONTH, startDate.getActualMinimum(Calendar.DAY_OF_MONTH));
-        } else if (length == Length.YEARLY){
+        } 
+        else if (length == Length.YEARLY){
             startDate.set(Calendar.DAY_OF_YEAR, startDate.getActualMinimum(Calendar.DAY_OF_YEAR));
         }
     }
@@ -113,14 +121,16 @@ public class TimeSpan {
         
         if (length == Length.WEEKLY){
             endDate.add(Calendar.DATE, 6);
-        } else if (length == Length.BIWEEKLY){
+        } 
+        else if (length == Length.BIWEEKLY){
             endDate.add(Calendar.DAY_OF_YEAR, 13);
-        } else if (length == Length.MONTHLY){
+        } 
+        else if (length == Length.MONTHLY){
             endDate.set(Calendar.DAY_OF_MONTH, endDate.getActualMaximum(Calendar.DAY_OF_MONTH));
-        } else if (length == Length.YEARLY){
+        } 
+        else if (length == Length.YEARLY){
             endDate.set(Calendar.DAY_OF_YEAR, endDate.getActualMaximum(Calendar.DAY_OF_YEAR));
         }
-        
     }
     
     private void clearDate(Calendar date){
